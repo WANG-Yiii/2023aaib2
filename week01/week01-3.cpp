6197a483c90dbbed9ec04d4e54a2adcb5cacb744
WANG-Yiii <naroragrace@gmail.com>
2024-02-23 11:57:28 +0800

    week01

---
+++ week01/week01-3.cpp
@@ -0,0 +1,8 @@
+///week01-3.cpp結合 Hello World 和 %d
+/// 在 CodeBlocks裡， File-New-Empty空白檔
+///再 CTRL-S 存檔成 WEEK01-3.CPP， 寫程式
+#include <stdio.h>
+int main()
+{
+    print("Hello World %d\n", 100);
+}
