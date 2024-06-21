382247805a6176faf6d67bb28d25bea8286ddef9
WANG-Yiii <naroragrace@gmail.com>
2024-06-21 08:34:10 +0800

    week03

--- week03/week03-3.cpp
+++ week03/week03-3.cpp
@@ -1,17 +1,16 @@
-
 ///week03-3.cpp
 #include <stdio.h>
 int main()
 {
-int a = 5;
-while(a>0){
-    printf("a�O%d\n",a);
-    a -= 1; ///�s�[:��a�C�C�ܤp
-    }
-for(int b=5; b>0; b -= 1){
-    printf("b�O%d\n",b);
-    }
-for(int i=0; i<5; i += 1){
-    printf("i�O%d\n",i);
+    int a = 5;
+    while(a>0){
+        printf("a¬O%d\n",a);
+            a -= 1; ///·s¥[:ÅýaºCºCÅÜ¤p
+        }
+    for(int b=5; b>0; b -= 1){
+        printf("b¬O%d\n",b);
+        }
+    for(int i=0; i<5; i += 1){
+        printf("i¬O%d\n",i);
     }
 }
